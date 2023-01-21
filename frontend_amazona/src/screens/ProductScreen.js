51b46befcf4561681e5e68baa0f71ba553da846a
Marion Terray <mterray@gmail.com>
2023-01-21 12:05:52 +0100

    create cart

--- frontend_amazona/src/screens/ProductScreen.js
+++ frontend_amazona/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
+import { Store } from '../Store';
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -50,6 +51,13 @@ useEffect(() => {
   fetchData();
 }, [slug]);
 
+const {state, dispatch: contextDispatch} = useContext(Store);
+const addToCartHandler = () => {
+    contextDispatch({
+        type: 'CART_ADD_ITEM', 
+        payload: {...product, quantity: 1 },
+  })
+}
 
   return (
     loading? (
@@ -109,7 +117,7 @@ useEffect(() => {
                   {product.countInStock > 0 && (
                     <ListGroup.Item>
                       <div className='d-grid'>
-                        <Button variant="primary">
+                        <Button onClick={addToCartHandler} variant="primary">
                           Add to Cart
                         </Button>
                       </div>
