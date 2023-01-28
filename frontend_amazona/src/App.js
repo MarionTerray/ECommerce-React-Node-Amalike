e113d6279f77dca4a8d0e4989e2e5cc799de05e3
Marion Terray <mterray@gmail.com>
2023-01-28 17:06:56 +0100

    complete sign in annd out process

--- frontend_amazona/src/App.js
+++ frontend_amazona/src/App.js
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,18 +11,25 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/esm/Badge';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
-  const {state } = useContext(Store);
-  const { cart } = state;
+  const {state, dispatch: contextDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    contextDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  }
 
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
+      <ToastContainer position='bottom-center' limit={1} />
       <header>
       <Navbar bg="dark" variant="dark" >
         <Container>
@@ -37,6 +45,27 @@ function App() {
                 </Badge>
               )}
             </Link>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>User Profile</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/orderhistory">
+                  <NavDropdown.Item>Order History</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <Link className='dropdown-item'
+                    to="#signout"
+                     onClick={signoutHandler}
+                >
+                Sign Out
+                </Link>
+              </NavDropdown>
+            ) : (
+              <Link className="nav-link" to="/signin">
+              Sign In 
+              </Link>
+            )}
           </Nav>
         </Container>
       </Navbar>
